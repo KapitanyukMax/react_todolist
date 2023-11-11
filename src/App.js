e228d4703984a0b426408448917531dde6419ece
maxik20192006max@gmail.com <maxik20192006max@gmail.com>
2023-11-11 10:06:45 +0200

    Initial

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function App() {
-  return (
-    <p>Hello React!</p>
-  );
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>To-Do List App</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, veniam!</p>
+    </div>
+  );
+}
+
+export default App;
